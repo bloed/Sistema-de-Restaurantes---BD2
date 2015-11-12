866038778a70f2def66fcaa5c3ec880230e49210
xelop <adrianlq8@hotmail.com>
2015-11-11 19:14:04 -0600

    inicio servidor y toda la interfaz, sin angularjs

--- server.js
+++ server.js
@@ -1,46 +1,33 @@
-var express = require('express');
-var app = express();
+var sql = require("mssql");
 
-var cookieParser = require('cookie-parser')
+var dbConfig = {
+  server: "XELOP-PC",
+  database: "Sistemas de Restaurantes",
+  user: "admin",
+  password: "admin",
+  port: 1433
+};
 
-app.use(cookieParser())
+function getEmp(){
+  var conn = new sql.Connection(dbConfig);
+  var req = new sql.Request(conn);
 
-app.get('/', function(req, res) {
-  console.log("Cookies: ", req.cookies)
-})
+  conn.connect(function(err){
+    if(err){
+      console.log(err);
+      return;
+    }
+    req.query("Select * from Ingredientes", function(err, recordset){
+      if(err){
+      console.log(err);
+      return;
+      }
+      else{
+        console.log(recordset);
+      }
+      conn.close();
+    });
+  });
+}
 
-
-// This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
-})
-
-// This responds a DELETE request for the /del_user page.
-app.delete('/del_user', function (req, res) {
-   console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-   console.log("Got a GET request for /list_user");
-   res.send('Page Listing');
-})
-
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-   console.log("Got a GET request for /ab*cd");
-   res.send('Page Pattern Match');
-})
-
-
-
-var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
+getEmp();
